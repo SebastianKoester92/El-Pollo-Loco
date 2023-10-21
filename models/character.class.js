0c8257e83975cd5a2f5bcb1b306e56ce98e0d9b2
Sebastian Köster <skoester.92@web.de>
2023-10-21 15:45:52 +0200

    first commit

---
+++ models/character.class.js
@@ -0,0 +1,17 @@
+class Character extends MovableObject {
+
+    height = 300;
+    y = 135; 
+
+
+    constructor() {
+        super().loadImage('../img/2_character_pepe/2_walk/W-21.png');
+    }
+
+
+
+
+    jump() {
+
+    }
+}
